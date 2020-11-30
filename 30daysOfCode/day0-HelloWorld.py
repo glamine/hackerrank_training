1080d2e0c44747ca9e87baba2a03adf083b123c7
glamine <guillaumelamine@hotmail.com>
2020-11-30 11:13:20 +0100

    starting coding challenge 3 first days

---
+++ 30daysOfCode/day0-HelloWorld.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 10:51:42 2020
+
+@author: guill
+"""
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+print(input_string)
---
+++ 30daysOfCode/day3-operators.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 11:06:08 2020
+
+@author: guill
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    
+    tip = meal_cost*tip_percent/100
+    tax = meal_cost*tax_percent/100
+    totalCost = int(meal_cost + tip + tax)
+    
+    print(totalCost)
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
