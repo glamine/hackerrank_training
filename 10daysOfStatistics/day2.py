bf1473e2c8e12cdae6fe9c489846f12f5362b9df
glamine <guillaumelamine@hotmail.com>
2020-12-06 23:18:50 +0100

    day2 - compound probability

--- 10daysOfStatistics/day2.py
+++ 10daysOfStatistics/day2.py
@@ -43,3 +43,17 @@ for i in range(len(dice1)):
 # Get probability
 probability = total_possibilities / total
 print("Probability: {0}/{1} = {2}".format(total_possibilities,total,probability))
+
+
+#%% Set data
+x = {"Red":4/7, "Black":3/7}
+y = {"Red":5/9, "Black":4/9}
+z = {"Red":4/8, "Black":4/8}
+
+# Get possibilities
+first_possibility = x["Red"] * y["Red"] * z["Black"]
+second_possibility = x["Red"] * y["Black"] * z["Red"]
+third_possibility = x["Black"] * y["Red"] * z["Red"]
+
+# Get probability
+print(first_possibility + second_possibility + third_possibility)
