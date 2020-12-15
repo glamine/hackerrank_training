868cff9db593c43888b6d048c67705aa9d64e7ca
glamine <guillaumelamine@hotmail.com>
2020-12-15 17:45:39 +0100

    day 15 - linked list

---
+++ 30daysOfCode/day15-linked_list.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 16:59:44 2020
+
+@author: guill
+"""
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+       
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+    
+        n = Node(data)    
+        
+        if head == None:
+            head = n
+            
+        else:
+            current = head
+            
+            while current.next: # attention risque de boucle infinie si mal gere
+                current = current.next
+            
+            current.next = n
+                
+        return head  
+    
+    # Solution recursive
+# =============================================================================
+#     def insert(self,head,data):
+#         n = Node(data)
+#         if head==None:
+#             head= n
+#         elif head.next == None:
+#             head.next = n
+#         else:
+#             self.insert(head.next,data)
+# 
+#         return head
+# =============================================================================
+    
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	 
